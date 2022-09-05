0806bb6c38c89d37153ff1d9fe3528cae3fcee07
Jens Nyberg <jens.nyberg@gmail.com>
2022-09-05 22:29:53 +0200

    first commit
    Signed-off-by: Jens Nyberg <jens.nyberg@gmail.com>

--- main.c
+++ main.c
@@ -3,10 +3,55 @@
 #include <math.h>
 
 #define NUMEPOCHS 10000
-#define NUMINPUTS 2
-#define NUMHIDDENNODES 2
-#define NUMOUTPUTS 1
+#define NUMNODELAYERS 3
+#define NUMCONNECTIONLAYERS 2
+#define LAYER0SIZE 2
+#define LAYER1SIZE 2
+#define LAYER2SIZE 1
 #define NUMTRAININGSETS 4
+#define LEARNINGRATE 0.1f
+
+struct node
+{
+
+    double bias;
+    double output;
+    double delta;
+
+};
+
+struct nodelayer
+{
+
+    struct node *nodes;
+    unsigned int size;
+
+};
+
+struct connection
+{
+
+    double weight;
+
+};
+
+struct connectionlayer
+{
+
+    struct connection *connections;
+    unsigned int size;
+
+};
+
+struct network
+{
+
+    struct nodelayer *nlayers;
+    unsigned int nsize;
+    struct connectionlayer *clayers;
+    unsigned int csize;
+
+};
 
 double sigmoid(double x)
 {
@@ -15,21 +60,21 @@ double sigmoid(double x)
 
 }
 
-double dSigmoid(double x)
+double sigmoid_derived(double x)
 {
 
     return x * (1 - x);
 
 }
 
-double init_weight()
+double randomize(void)
 {
 
     return ((double)rand()) / ((double)RAND_MAX);
 
 }
 
-void shuffle(int *array, unsigned int n)
+static void shuffle(unsigned int *a, unsigned int n)
 {
 
     unsigned int i;
@@ -38,189 +83,371 @@ void shuffle(int *array, unsigned int n)
     {
 
         unsigned int j = i + rand() / (RAND_MAX / (n - i) + 1);
-        int t = array[j];
+        unsigned int t = a[j];
 
-        array[j] = array[i];
-        array[i] = t;
+        a[j] = a[i];
+        a[i] = t;
 
     }
+
+}
+
+static void setinputs(struct nodelayer *layer, double *inputs)
+{
+
+    unsigned int j;
+
+    for (j = 0; j < layer->size; j++)
+    {
+
+        struct node *node = &layer->nodes[j];
+
+        node->output = inputs[j];
+
+    }
+
+}
+
+static void forwardprop(struct nodelayer *layerA, struct nodelayer *layerB, struct connectionlayer *clayer)
+{
+
+    unsigned int j;
+
+    for (j = 0; j < layerB->size; j++)
+    {
+
+        struct node *nodeB = &layerB->nodes[j];
+        double activation = nodeB->bias;
+        unsigned int k;
+
+        for (k = 0; k < layerA->size; k++)
+        {
+
+            struct node *nodeA = &layerA->nodes[k];
+            struct connection *connection = &clayer->connections[k * layerB->size + j];
+
+            activation += nodeA->output * connection->weight;
+
+        }
+
+        nodeB->output = sigmoid(activation);
+
+    }
+
+}
+
+static void setoutputs(struct nodelayer *layer, double *outputs)
+{
+
+    unsigned int j;
+
+    for (j = 0; j < layer->size; j++)
+    {
+
+        struct node *node = &layer->nodes[j];
+        double error = outputs[j] - node->output;
+
+        node->delta = error * sigmoid_derived(node->output);
+
+    }
+
+}
+
+static void backprop(struct nodelayer *layerA, struct nodelayer *layerB, struct connectionlayer *clayer)
+{
+
+    unsigned int j;
+
+    for (j = 0; j < layerB->size; j++)
+    {
+
+        struct node *nodeB = &layerB->nodes[j];
+        double error = 0.0f;
+        unsigned int k;
+
+        for (k = 0; k < layerA->size; k++)
+        {
+
+            struct node *nodeA = &layerA->nodes[k];
+            struct connection *connection = &clayer->connections[j * layerA->size + k];
+
+            error += nodeA->delta * connection->weight;
+
+        }
+
+        nodeB->delta = error * sigmoid_derived(nodeB->output);
+
+    }
+
+    for (j = 0; j < layerA->size; j++)
+    {
+
+        struct node *nodeA = &layerA->nodes[j];
+        unsigned int k;
+
+        nodeA->bias += nodeA->delta * LEARNINGRATE;
+
+        for (k = 0; k < layerB->size; k++)
+        {
+
+            struct node *nodeB = &layerB->nodes[k];
+            struct connection *connection = &clayer->connections[k * layerA->size + j];
+
+            connection->weight += nodeB->output * nodeA->delta * LEARNINGRATE;
+
+        }
+
+    }
+
+}
+
+static void node_init(struct node *node)
+{
+
+    node->bias = randomize();
+    node->output = 0.0f;
+    node->delta = 0.0f;
+
+}
+
+static void nodelayer_init(struct nodelayer *layer, unsigned int size)
+{
+
+    layer->nodes = 0;
+    layer->size = size;
+
+}
+
+static void nodelayer_create(struct nodelayer *layer)
+{
+
+    unsigned int i;
+
+    layer->nodes = malloc(sizeof (struct node) * layer->size);
+
+    for (i = 0; i < layer->size; i++)
+        node_init(&layer->nodes[i]);
+
+}
+
+static void nodelayer_destroy(struct nodelayer *layer)
+{
+
+    free(layer->nodes);
+
+    layer->nodes = 0;
+
+}
+
+static void connection_init(struct connection *connection)
+{
+
+    connection->weight = randomize();
+
+}
+
+static void connectionlayer_init(struct connectionlayer *layer, unsigned int size)
+{
+
+    layer->connections = 0;
+    layer->size = size;
+
+}
+
+static void connectionlayer_create(struct connectionlayer *layer)
+{
+
+    unsigned int i;
+
+    layer->connections = malloc(sizeof (struct connection) * layer->size);
+
+    for (i = 0; i < layer->size; i++)
+        connection_init(&layer->connections[i]);
+
+}
+
+static void connectionlayer_destroy(struct connectionlayer *layer)
+{
+
+    free(layer->connections);
+
+    layer->connections = 0;
+
+}
+
+static void network_init(struct network *network, struct nodelayer *nlayers, unsigned int nsize, struct connectionlayer *clayers, unsigned int csize)
+{
+
+    network->nlayers = nlayers;
+    network->nsize = nsize;
+    network->clayers = clayers;
+    network->csize = csize;
+
+}
+
+static void network_create(struct network *network)
+{
+
+    unsigned int i;
+
+    for (i = 0; i < network->nsize; i++)
+        nodelayer_create(&network->nlayers[i]);
+
+    for (i = 0; i < network->csize; i++)
+        connectionlayer_create(&network->clayers[i]);
+
+}
+
+static void network_destroy(struct network *network)
+{
+
+    unsigned int i;
+
+    for (i = 0; i < network->nsize; i++)
+        nodelayer_destroy(&network->nlayers[i]);
+
+    for (i = 0; i < network->csize; i++)
+        connectionlayer_destroy(&network->clayers[i]);
+
+}
+
+static void forwardpass(struct network *network, double *inputs)
+{
+
+    unsigned int n;
+
+    setinputs(&network->nlayers[0], inputs);
+
+    for (n = 0; n < network->nsize; n++)
+        forwardprop(&network->nlayers[n], &network->nlayers[n + 1], &network->clayers[n]);
+
+}
+
+static void backwardpass(struct network *network, double *outputs)
+{
+
+    unsigned int n;
+
+    setoutputs(&network->nlayers[network->nsize - 1], outputs);
+
+    for (n = network->nsize - 1; n > 0; n--)
+        backprop(&network->nlayers[n], &network->nlayers[n - 1], &network->clayers[n - 1]);
+
+}
+
+static void train(struct network *network)
+{
+
+    double training_inputs[NUMTRAININGSETS * LAYER0SIZE] = {
+        0.0f, 0.0f,
+        1.0f, 0.0f,
+        0.0f, 1.0f,
+        1.0f, 1.0f
+    };
+    double training_outputs[NUMTRAININGSETS * LAYER2SIZE] = {
+        0.0f,
+        1.0f,
+        1.0f,
+        0.0f
+    };
+    unsigned int training_order[NUMTRAININGSETS];
+    unsigned int epoch;
+    unsigned int i;
+
+    for (i = 0; i < NUMTRAININGSETS; i++)
+        training_order[i] = i;
+
+    for (epoch = 0; epoch < NUMEPOCHS; epoch++)
+    {
+
+        unsigned int i;
+
+        shuffle(training_order, NUMTRAININGSETS);
+
+        for (i = 0; i < NUMTRAININGSETS; i++)
+        {
+
+            unsigned int setindex = training_order[i];
+            double *inputs = training_inputs + setindex * network->nlayers[0].size;
+            double *outputs = training_outputs + setindex * network->nlayers[2].size;
+
+            forwardpass(network, inputs);
+            backwardpass(network, outputs);
+
+        }
+
+    }
+
+}
+
+static void validatehelper(struct network *network, double *input, double answer)
+{
+
+    double confidence;
+    double distance;
+
+    forwardpass(network, input);
+
+    confidence = network->nlayers[2].nodes[0].output;
+    distance = fabs(confidence - answer);
+
+    printf("Validating test:\n");
+    printf("  Confidence %f\n", confidence);
+
+    if (distance < 0.5)
+        printf("  Prediction: Correct\n");
+    else
+        printf("  Prediction: Wrong\n");
+
+}
+
+static void validate(struct network *network)
+{
+
+    static double input1[LAYER0SIZE] = {
+        0.0f, 0.0f
+    };
+    static double input2[LAYER0SIZE] = {
+        1.0f, 1.0f
+    };
+    static double input3[LAYER0SIZE] = {
+        1.0f, 0.0f
+    };
+    static double input4[LAYER0SIZE] = {
+        0.0f, 1.0f
+    };
+
+    validatehelper(network, input1, 0.0f);
+    validatehelper(network, input2, 0.0f);
+    validatehelper(network, input3, 1.0f);
+    validatehelper(network, input4, 1.0f);
+
+}
+
+static struct nodelayer nodelayers[NUMNODELAYERS];
+static struct connectionlayer connectionlayers[NUMCONNECTIONLAYERS];
+static struct network network;
+
+static void init(void)
+{
+
+    nodelayer_init(&nodelayers[0], LAYER0SIZE);
+    nodelayer_init(&nodelayers[1], LAYER1SIZE);
+    nodelayer_init(&nodelayers[2], LAYER2SIZE);
+    connectionlayer_init(&connectionlayers[0], LAYER0SIZE * LAYER1SIZE);
+    connectionlayer_init(&connectionlayers[1], LAYER1SIZE * LAYER2SIZE);
+    network_init(&network, nodelayers, NUMNODELAYERS, connectionlayers, NUMCONNECTIONLAYERS);
 
 }
 
 int main(int argc, const char **argv)
 {
 
-    const double lr = 0.1f;
-    int trainingSetOrder[NUMTRAININGSETS] = {0, 1, 2, 3};
-    double hiddenLayer[NUMHIDDENNODES];
-    double outputLayer[NUMOUTPUTS];
-    double hiddenLayerBias[NUMHIDDENNODES];
-    double outputLayerBias[NUMOUTPUTS];
-    double hiddenWeights[NUMINPUTS][NUMHIDDENNODES];
-    double outputWeights[NUMHIDDENNODES][NUMOUTPUTS];
-    double training_inputs[NUMTRAININGSETS][NUMINPUTS] = {
-        {0.0f, 0.0f},
-        {1.0f, 0.0f},
-        {0.0f, 1.0f},
-        {1.0f, 1.0f}
-    };
-    double training_outputs[NUMTRAININGSETS][NUMOUTPUTS] = {
-        {0.0f},
-        {1.0f},
-        {1.0f},
-        {0.0f}
-    };
-
-    for (int i = 0; i < NUMINPUTS; i++)
-        for (int j = 0; j < NUMHIDDENNODES; j++)
-            hiddenWeights[i][j] = init_weight();
-
-    for (int i = 0; i < NUMHIDDENNODES; i++)
-    {
-
-        hiddenLayerBias[i] = init_weight();
-
-        for (int j = 0; j < NUMOUTPUTS; j++)
-            outputWeights[i][j] = init_weight();
-
-    }
-
-    for (int i = 0; i < NUMOUTPUTS; i++)
-        outputLayerBias[i] = init_weight();
-
-    for (int n = 0; n < NUMEPOCHS; n++)
-    {
-
-        shuffle(trainingSetOrder, NUMTRAININGSETS);
-
-        for (int x = 0; x < NUMTRAININGSETS; x++)
-        {
-
-            int i = trainingSetOrder[x];
-
-            // Forward pass
-
-            for (int j = 0; j < NUMHIDDENNODES; j++)
-            {
-
-                double activation = hiddenLayerBias[j];
-
-                for (int k = 0; k < NUMINPUTS; k++)
-                    activation += training_inputs[i][k] * hiddenWeights[k][j];
-
-                hiddenLayer[j] = sigmoid(activation);
-
-            }
-
-            for (int j = 0; j < NUMOUTPUTS; j++)
-            {
-
-                double activation=outputLayerBias[j];
-
-                for (int k = 0; k < NUMHIDDENNODES; k++)
-                    activation += hiddenLayer[k] * outputWeights[k][j];
-
-                outputLayer[j] = sigmoid(activation);
-
-            }
-
-            printf("Input\n");
-            printf("  %f\n", training_inputs[i][0]);
-            printf("  %f\n", training_inputs[i][1]);
-            printf("Output\n");
-            printf("  %f\n", outputLayer[0]);
-            printf("Expected output\n");
-            printf("  %f\n", training_outputs[i][0]);
-
-            // Backprop
-
-            double deltaOutput[NUMOUTPUTS];
-
-            for (int j = 0; j < NUMOUTPUTS; j++)
-            {
-
-                double errorOutput = training_outputs[i][j] - outputLayer[j];
-
-                deltaOutput[j] = errorOutput * dSigmoid(outputLayer[j]);
-
-            }
-
-            double deltaHidden[NUMHIDDENNODES];
-
-            for (int j = 0; j < NUMHIDDENNODES; j++)
-            {
-
-                double errorHidden = 0.0f;
-
-                for (int k = 0; k < NUMOUTPUTS; k++)
-                    errorHidden += deltaOutput[k] * outputWeights[j][k];
-
-                deltaHidden[j] = errorHidden*dSigmoid(hiddenLayer[j]);
-
-            }
-
-            for (int j = 0; j < NUMOUTPUTS; j++)
-            {
-
-                outputLayerBias[j] += deltaOutput[j] * lr;
-
-                for (int k = 0; k < NUMHIDDENNODES; k++)
-                    outputWeights[k][j] += hiddenLayer[k] * deltaOutput[j] * lr;
-
-            }
-
-            for (int j = 0; j < NUMHIDDENNODES; j++)
-            {
-
-                hiddenLayerBias[j] += deltaHidden[j] * lr;
-
-                for (int k = 0; k < NUMINPUTS; k++)
-                    hiddenWeights[k][j] += training_inputs[i][k] * deltaHidden[j] * lr;
-
-            }
-
-        }
-
-    }
-
-    printf("Final Hidden Weights\n");
-    printf("[\n");
-
-    for (int j = 0; j < NUMHIDDENNODES; j++)
-    {
-
-        for (int k = 0; k < NUMINPUTS; k++)
-            printf("  %f\n", hiddenWeights[k][j]);
-
-    }
-
-    printf("]\n");
-    printf("Final Hidden Biases\n");
-    printf("[\n");
-
-    for (int j = 0; j < NUMHIDDENNODES; j++)
-        printf("  %f\n", hiddenLayerBias[j]);
-
-    printf("]\n");
-    printf("Final Output Weights\n");
-    printf("[\n");
-
-    for (int j = 0; j < NUMOUTPUTS; j++)
-    {
-
-        for (int k = 0; k < NUMHIDDENNODES; k++)
-            printf("  %f\n", outputWeights[k][j]);
-
-    }
-
-    printf("]\n");
-    printf("Final Output Biases\n");
-    printf("[\n");
-
-    for (int j = 0; j < NUMOUTPUTS; j++)
-        printf("  %f\n", outputLayerBias[j]);
-
-    printf("]\n");
+    init();
+    network_create(&network);
+    train(&network);
+    validate(&network);
+    network_destroy(&network);
 
     return 0;
 }
